6f2e7c0db53a5b6a83b27e282961e32b0bd4035d
Rafael Ascensão <rafa.almas@gmail.com>
2021-04-04 11:32:16 +0100

    Notify the player the current game session length
    Due to technical limitations of the modding API, it is not trivial to
    determine the session length of the player hosting a multiplayer server
    or playing in single player mode.
    
    Players in such categories should reset the timer manually by issuing:
        /wellbeing reset-timer
    at the start of each session.
    
    For the remaining players the timer is reset automatically when the
    player joins the game.

--- control.lua
+++ control.lua
@@ -1,3 +1,9 @@
+function formattime(ticks)
+    local minutes = math.floor(ticks / 60 / 60)
+    local hours = math.floor(minutes / 60)
+    return string.format("%02dh%02dm", hours, minutes % 60)
+end
+
 function wellbeing_message_handler()
     local ticks_per_minute = 60 * 60
 
@@ -14,12 +20,33 @@ function wellbeing_message_handler()
         function(event)
             for i, player in pairs(game.players) do
                 if player.mod_settings["wellbeing-alert-interval"].value * ticks_per_minute == event.nth_tick then
-                    player.print("Perhaps time for a small pause?")
+                    local session_duration = game.tick - (global.session_start[player.index] or 0)
+                    player.print("You've been playing for " .. formattime(session_duration) .. "\n" ..
+                                 "Perhaps time for a pause? Will warn again in: " .. formattime(event.nth_tick))
                 end
             end
         end
     )
 end
 
-script.on_init(wellbeing_message_handler)
+script.on_init(
+    function()
+        global.session_start = {}
+        wellbeing_message_handler()
+    end
+)
+
+script.on_event(defines.events.on_player_joined_game,
+    function(event)
+        global.session_start[event.player_index] = game.tick
+    end
+)
+
+commands.add_command("wellbeing", "<reset-timer> resets the session timer to zero",
+    function(command)
+        if command.parameter == "reset-timer" then
+            global.session_start[command.player_index] = game.tick
+        end
+    end
+)
 script.on_event(defines.events.on_runtime_mod_setting_changed, wellbeing_message_handler)
